3bb623b1932dd888e764b6e5ac53b055fd360a9e
Tom Gelhorn <tom.gelhorn@gmail.com>
2026-02-23 13:28:15 +0100

    added working IMU douple Tap detection

---
+++ working_code/imu_i2c_interrupt.cpp
@@ -0,0 +1,109 @@
+#include <Wire.h>
+#include "SparkFun_BMA400_Arduino_Library.h"
+
+// Create a new sensor object
+BMA400 accelerometer;
+
+// I2C address selection
+uint8_t i2cAddress = BMA400_I2C_ADDRESS_DEFAULT; // 0x14
+//uint8_t i2cAddress = BMA400_I2C_ADDRESS_SECONDARY; // 0x15
+
+// Pin used for interrupt detection
+int interruptPin = D7;
+
+// Flag to know when interrupts occur
+volatile bool interruptOccurred = false;
+
+
+void bma400InterruptHandler()
+{
+    interruptOccurred = true;
+}
+
+void setup()
+{
+    // Start serial
+    Serial.begin(115200);
+    Serial.println("BMA400 Example 9 - Tap Detection");
+    pinMode(interruptPin, INPUT);
+    // Initialize the I2C library
+    Wire.begin();
+
+    // Check if sensor is connected and initialize
+    // Address is optional (defaults to 0x14)
+    while(accelerometer.beginI2C(i2cAddress) != BMA400_OK)
+    {
+        // Not connected, inform user
+        Serial.println("Error: BMA400 not connected, check wiring and I2C address!");
+
+        // Wait a bit to see if connection is established
+        delay(1000);
+    }
+
+    Serial.println("BMA400 connected!");
+
+    // The tap detection requires a 200Hz ODR (default)
+    accelerometer.setODR(BMA400_ODR_200HZ);
+    
+    // Increasing the range from the default of 4g to the max of 16g helps
+    // capture the large acceleration spikes caused by tapping
+    accelerometer.setRange(BMA400_RANGE_16G);
+    
+    // Here we configure the tap detection feature of the BMA400. It can detect
+    // both single and double taps as a form of user input. There are a number
+    // of parameters than can be configured to help distinguish taps from other
+    // sources of noise.
+    bma400_tap_conf config =
+    {
+        .axes_sel = BMA400_TAP_Z_AXIS_EN, // Which axes to evaluate for interrupts (X/Y/Z in any combination)
+        .sensitivity = BMA400_TAP_SENSITIVITY_0, // Sensitivity threshold, up to 7 (lower is more sensitive)
+        .tics_th = BMA400_TICS_TH_18_DATA_SAMPLES, // Max time between top/bottom peaks of a single tap
+        .quiet = BMA400_QUIET_60_DATA_SAMPLES, // Minimum time between taps to trigger interrupt
+        .quiet_dt = BMA400_QUIET_DT_4_DATA_SAMPLES, // Minimum time between 2 taps to trigger double tap interrupt
+        .int_chan = BMA400_INT_CHANNEL_1 // Which pin to use for interrupts
+    };
+    accelerometer.setTapInterrupt(&config);
+    
+    // Here we configure the INT1 pin to push/pull mode, active high
+    accelerometer.setInterruptPinMode(BMA400_INT_CHANNEL_1, BMA400_INT_PUSH_PULL_ACTIVE_1);
+    
+    // Enable single tap interrupt condition
+    accelerometer.enableInterrupt(BMA400_SINGLE_TAP_INT_EN, true);
+    
+    // Enable double tap interrupt condition
+    accelerometer.enableInterrupt(BMA400_DOUBLE_TAP_INT_EN, true);
+    
+    // Setup interrupt handler
+    attachInterrupt(digitalPinToInterrupt(interruptPin), bma400InterruptHandler, RISING);
+}
+
+void loop()
+{
+    // Wait for interrupt to occur
+    if(interruptOccurred)
+    {
+        // Reset flag for next interrupt
+        interruptOccurred = false;
+
+        Serial.print("Interrupt occurred!");
+        Serial.print("\t");
+
+        // Get the interrupt status to know which condition triggered
+        uint16_t interruptStatus = 0;
+        accelerometer.getInterruptStatus(&interruptStatus);
+        
+        // Check if this is the tap interrupt condition
+        if(interruptStatus & BMA400_ASSERTED_S_TAP_INT)
+        {
+            Serial.println("Single tap!");
+        }
+        else if(interruptStatus & BMA400_ASSERTED_D_TAP_INT)
+        {
+            Serial.println("Double tap!");
+        }
+        else
+        {
+            Serial.println("Wrong interrupt condition!");
+        }
+    }
+}
